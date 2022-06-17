23c9a94ccba9cc24a7a65884b129d3e19d63604c
qompadres <qompandre@misena.edu.co>
2022-06-16 22:54:49 -0500

    added pruebas 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-/* import { useState } from "react";
+ import { useState } from "react";
 import { Formik, Form, Field } from "formik";
+import Input from './components/Input'
 import './header.css'
-const App = () => {
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+const App = ({ signOut, user }) => {
+  const handleSubmit = () => {}
   const [photos, setPhotos] = useState([])
   const open = url => window.open(url)
   //console.log(photos);
   return (
+    <>
+    <h1>Helou Mr {user.username}</h1>
+    <button onClick={signOut}>Sign out</button>
     <div>
       <header>
         <Formik
-          initialValues={{search: ''}}
-          onSubmit={async values => {
+          initialValues={{cedula: '', credito: '',}}
+          /*onSubmit={async values => {
             const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
               headers: {
                 'Authorization': 'Client-ID J3XgWsqqG8-VyCpIcp9R-ngrmKSj2M66dQJRS3Wfh60'
@@ -19,13 +30,12 @@ const App = () => {
             const data = await response.json()
             // llamar a api de celya
             setPhotos(data.results)
-          }}
+          }}*/
+          onSubmit={handleSubmit}
         >
           <Form>
-            <Field name="search" />
-            <Field name="credito" />
-            <Field name="solicitud" />
-            <Field name="Enviar" /> 
+            <input name="cedula" label="Número de cedula"/>
+            <input name="credito" label="Número de crédito"/>
           </Form>  
         </Formik>
       </header>
@@ -39,14 +49,18 @@ const App = () => {
         </div>
       </div>
     </div>
+    </>
   )
 }
-export default App;*/
-import { Amplify } from 'aws-amplify';
+//export default App;
+export default withAuthenticator(App);
 
+/*
+import { Formik, Form, Field } from 'formik';
+import './header.css'
+import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
@@ -55,8 +69,23 @@ function App({ signOut, user }) {
     <>
       <h1>Helou Mr {user.username}</h1>
       <button onClick={signOut}>Sign out</button>
+      <div>
+        <Formik
+          initialValues={{search: '' }}
+          onSubmit={async values => {
+            // llamar a api de unsplash
+            console.log(values)
+          }}
+        >
+          <Form>
+            <Field name="search"/>
+            <Field name="credito"/>
+          </Form>  
+        </Formik>  
+      </div>
     </>
+    
   );
 }
 
-export default withAuthenticator(App);
+export default withAuthenticator(App); */
---
+++ src/components/Input.js
@@ -0,0 +1,8 @@
+//import styled from 'styled-components'
+import { useField } from 'formik'
+
+const Input = ({label, ...props}) => {
+    return 'dfdfg'
+}
+
+export default Input
