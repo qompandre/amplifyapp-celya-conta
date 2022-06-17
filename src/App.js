7645e22a5905abb95e027066de0b3f30f938a19a
qompadres <qompandre@misena.edu.co>
2022-06-16 21:39:44 -0500

    added pruebas

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ Amplify.configure(awsExports);
 function App({ signOut, user }) {
   return (
     <>
-      <h1>Hello {user.username}</h1>
+      <h1>Helou Mr {user.username}</h1>
       <button onClick={signOut}>Sign out</button>
     </>
   );
